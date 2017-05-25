f3a86258554a892bdab28530c8683a1a035ed0ea
gu1lh3rme <a14012@bcc.unifal-mg.edu>
2017-05-25 19:38:34 -0300

    Adição da função multiplicação

--- calculadora.c
+++ calculadora.c
@@ -9,8 +9,8 @@ int subtrai(int x, int y){
 	return x-y;
 }
 
-int multiplica(){
-	
+int multiplica(int x, int y){
+	return x*y;
 }
 
 int divide(){
@@ -21,6 +21,7 @@ int main(){
 	printf("Hello git!");
 	printf("Soma: %d\n",soma(2,2));
 	printf("Subtração: %d\n",subtrai(5,2));
+	printf("Multiplica: %d\n",multiplica(5,2));
 	printf("\n");
 	return 0;
 }
