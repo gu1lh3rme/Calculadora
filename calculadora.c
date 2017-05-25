c8e2af8ad1f154f5311246d4453c6fdb03b09b81
gu1lh3rme <a14012@bcc.unifal-mg.edu>
2017-05-25 19:27:12 -0300

    Adição da função subtrai

--- calculadora.c
+++ calculadora.c
@@ -5,8 +5,8 @@ int soma(int x, int y){
 	return x+y;	//Alteração feita na branch fSoma
 }
 
-int subtrai(){
-	
+int subtrai(int x, int y){
+	return x-y;
 }
 
 int multiplica(){
@@ -20,6 +20,7 @@ int divide(){
 int main(){
 	printf("Hello git!");
 	printf("Soma: %d\n",soma(2,2));
+	printf("Subtração: %d\n",subtrai(5,2));
 	printf("\n");
 	return 0;
 }
